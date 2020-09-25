ff73ac78aa14bc88adf5dd4655cc8162ec054aab
Xi <xwei@utexas.edu>
2020-09-25 11:35:20 -0700

    Launch LibActivity from MainActivity

--- app/src/main/java/com/xizz/resourceid/MainActivity.kt
+++ app/src/main/java/com/xizz/resourceid/MainActivity.kt
@@ -1,5 +1,17 @@
 package com.xizz.resourceid
 
+import android.content.Intent
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.xizz.library.LibActivity
+import kotlinx.android.synthetic.main.activity_main.launch_button
 
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        launch_button.setOnClickListener {
+            startActivity(Intent(this, LibActivity::class.java))
+        }
+    }
+}
